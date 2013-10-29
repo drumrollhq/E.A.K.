9f567eb024f643a915fa3a2d9895d6aca66fdedc
Joe Dytrych <me@jdytrych.co.uk>
2013-10-29 09:42:38 +0000

    Remove some old stuff

--- test/ScriptLoader.js
+++
@@ -1,3 +0,0 @@
-document.write('<script src="http://j.dytry.ch/downloads/js/chai.js"></script>');
-document.write('<script src="http://j.dytry.ch/downloads/js/sinon-1.7.3.js"></script>');
-document.write('<script src="http://j.dytry.ch/downloads/js/sinon-chai.js"></script>');
