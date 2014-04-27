2a84911b2f285bc8bb13ea85e4752ffdf04def2a
Joe Dytrych <me@jdytrych.co.uk>
2014-04-27 20:35:10 +0100

    gofmt

--- server/main.go
+++ server/main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "bytes"
-  "log"
-  "net/http"
-  "os/exec"
-  "strings"
+	"bytes"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
 
-  "github.com/zenazn/goji"
+	"github.com/zenazn/goji"
 )
 
 var APP_VERSION string
 
 func main() {
-  APP_VERSION = getVersion()
+	APP_VERSION = getVersion()
 
-  goji.Get("/*", static("../public"))
+	goji.Get("/*", static("../public"))
 
-  log.Println("Version:", APP_VERSION)
-  goji.Serve()
-  log.Println("Finished")
+	log.Println("Version:", APP_VERSION)
+	goji.Serve()
+	log.Println("Finished")
 }
 
-func NotFound (w http.ResponseWriter, r *http.Request) {
-    http.Error(w, "404 :(", http.StatusNotFound)
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "404 :(", http.StatusNotFound)
 }
 
 func getVersion() string {
-  v := run("git", "rev-parse", "HEAD")
-  if run("git", "status", "--porcelain") != "" {
-    v += "_DEV"
-  }
-  return v
+	v := run("git", "rev-parse", "HEAD")
+	if run("git", "status", "--porcelain") != "" {
+		v += "_DEV"
+	}
+	return v
 }
 
 func run(name string, args ...string) string {
-  cmd := exec.Command(name, args...)
-  var out bytes.Buffer
-  cmd.Stdout = &out
-  if err := cmd.Run(); err != nil {
-    log.Fatal(err)
-  }
-  return strings.Trim(out.String(), " \t\n\r")
+	cmd := exec.Command(name, args...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	return strings.Trim(out.String(), " \t\n\r")
 }
